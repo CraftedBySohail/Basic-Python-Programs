1c490107fdce9c3e6594a314448c0215c8a1d742
Sohail Khan <sohailkhanwps@gmail.com>
2025-02-02 12:10:37 +0530

    file added

---
+++ tuples/first.py
@@ -0,0 +1,17 @@
+# tuple is a immutable datatype 
+# it is used enclosed in () bracket
+# elements are seaparated using commas
+# we can acces element in python through index but cannot modify it 
+
+hello = (1,2,4,5,6,3,2,21) 
+print(type(hello))
+
+# empty tupple
+hello1 = ()
+print(hello1)
+
+
+# slicing in tuple is same as list
+print(hello[1:4])
+
+print(hello[:])
